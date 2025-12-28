a280316032477953f1c63b9dab09389edd2d6019
Manmeet Kaur <mkmanmeet6649@gmail.com>
2025-12-28 14:41:10 +0530

    Create 2235_add_two_integers.py

---
+++ arrays/2235_add_two_integers.py
@@ -0,0 +1,12 @@
+"""
+LeetCode 2235 – Add Two Integers
+
+Approach:
+- Return the sum of the two given integers
+
+"""
+class Solution(object):
+    def sum(self, num1, num2):
+        return num1+num2
+       
+        
