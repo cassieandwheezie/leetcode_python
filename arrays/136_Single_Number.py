c2278d77a35f999c7ef7b99011999a3fc44a9d69
Manmeet Kaur <mkmanmeet6649@gmail.com>
2025-12-30 19:51:20 +0530

    Create 136_Single_Number.py
    Adding solution of LeetCode-136

---
+++ arrays/136_Single_Number.py
@@ -0,0 +1,16 @@
+'''
+First loop → build frequency
+
+Second loop → find the unique one
+
+'''
+class Solution:
+    def singleNumber(self, nums: List[int]) -> int:
+        freq = {}
+
+        for num in nums:
+            freq[num] = freq.get(num, 0) + 1
+
+        for num, count in freq.items():
+            if count == 1:
+                return num
