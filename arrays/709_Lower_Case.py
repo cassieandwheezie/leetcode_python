79c5a75b302fceeccd4d299064ecc948b165d02e
Manmeet Kaur <mkmanmeet6649@gmail.com>
2025-12-31 15:35:16 +0530

    Create 709_Lower_Case.py
    Adding Solution of LeetCode-709 Converting a string to lower case

---
+++ arrays/709_Lower_Case.py
@@ -0,0 +1,8 @@
+'''
+s.upper() and s.lower() are the functions in Python to convert a string in upper case and lower case respectively
+'''
+class Solution:
+    def toLowerCase(self, s: str) -> str:
+        s=s.lower()
+        return s
+        
