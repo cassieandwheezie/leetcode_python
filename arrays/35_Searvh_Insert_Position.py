f2d06f8bca74411a81d2a6a28551dbbabfd04298
Manmeet Kaur <mkmanmeet6649@gmail.com>
2026-01-10 18:22:40 +0530

    Create 35_Search_Insert_Position.py
    Adding Solution of Search Insert Position LeetCode -35

---
+++ arrays/35_Searvh_Insert_Position.py
@@ -0,0 +1,16 @@
+class Solution:
+    def searchInsert(self, nums: List[int], target: int) -> int:
+        low = 0
+        high = len(nums)-1
+        while low<=high:
+            middle = (low+high)//2
+            if target < nums[middle]:
+                high = middle-1
+            elif target > nums[middle]:
+                low = middle+1
+            else:
+                return middle 
+        return low 
+    
+                    
+        
