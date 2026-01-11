4f9afa56b5f58411b98396757e07850cb0be5c73
Manmeet Kaur <mkmanmeet6649@gmail.com>
2026-01-11 11:49:19 +0530

    Update 278_First_Bad_Version.py

--- arrays/278_First_Bad_Version.py
+++ arrays/278_First_Bad_Version.py
@@ -1,11 +1,31 @@
 # The isBadVersion API is already defined for you.
 # def isBadVersion(version: int) -> bool:
-
+'''
 class Solution:
     def firstBadVersion(self, n: int) -> int:
         for i in range(1,n+1):
             if isBadVersion(i):
                 return i
+This is the correct solution but exceeds the time lmit 
+So, the best way to is to binary search 
+'''
+
+# The isBadVersion API is already defined for you.
+# def isBadVersion(version: int) -> bool:
+
+class Solution:
+    def firstBadVersion(self, n: int) -> int:
+        low = 1
+        high = n 
+        while low <= high:
+            mid = (low + high) // 2
+            if isBadVersion(mid):
+                high = mid - 1
+            else:
+                low = mid + 1
+        return low
 
 
+
+        
         
